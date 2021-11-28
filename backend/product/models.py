a6cace3272de4d58e2609595cd0701ab93d079e5
Yii-xiii <yinxuann1357@gmail.com>
2021-11-29 00:04:46 +0800

    added basic views

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def body(self):
+        return {'id': self.id,
+                'title': self.title,
+                'description': self.description,
+                }
--- backend/product/views.py
+++ backend/product/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from .models import Product
+from django.http import JsonResponse
+import json
+
 
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return JsonResponse([dict(product.body()) for product in products], safe = False)
+
+
+def create_product(request):
+    if request.method == 'POST':
+        product = Product.objects.create()
+        data = json.loads(request.body)
+        product.title = data["title"]
+        product.description = data["description"]
+        product.save()
+        products = Product.objects.all()
+        return JsonResponse([dict(product.body()) for product in products], safe=False)
+
+
+def product(request, pk):
+    if request.method == 'DELETE':
+        product = Product.objects.get(id=pk)
+        product.delete()
+        products = Product.objects.all()
+        return JsonResponse([dict(product.body()) for product in products], safe = False)
+    elif request.method == 'GET':
+        products = Product.objects.filter(id=pk)
+        return JsonResponse([dict(product.body()) for product in products], safe = False)
+    elif request.method == 'PUT':
+        product = Product.objects.get(id=pk)
+        data = json.loads(request.body)
+        product.title = data["title"]
+        product.description = data["description"]
+        product.save()
+        products = Product.objects.all()
+        return JsonResponse([dict(product.body()) for product in products], safe = False)
