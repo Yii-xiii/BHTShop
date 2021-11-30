c28ea3f73f8bb89533e06bf209aeda9059cc7cff
Yii-xiii <yinxuann1357@gmail.com>
2021-11-30 11:53:17 +0800

    updated api

--- frontend/src/Components/Api.js
+++ frontend/src/Components/Api.js
@@ -46,9 +46,19 @@ class Api {
 		}
 	}	
 
-
+	//Product CRUD
 	getProductList = () => {
 		let data = this.get(`/products/`);
+		return data;
+	}
+
+	getProductListByPageNum = (pageNum) => { //10 per page
+		let data = this.get(`/products/pages/${pageNum}`);
+		return data;
+	}
+
+	getLastestProductListByPageNum = (pageNum) => { //10 per page
+		let data = this.get(`/products/latest/pages/${pageNum}`);
 		return data;
 	}
 
@@ -72,6 +82,44 @@ class Api {
 		return data;
 	}
 
+	//ProductSpec CRUD
+	getProductSpecList = (productId) => {
+		let data = this.get(`/products/${productId}/specs`);
+		return data;
+	}
+
+	getProductSpecListByPageNum = (productId,pageNum) => { //10 per page
+		let data = this.get(`/products/${productId}/specs/pages/${pageNum}`);
+		return data;
+	}
+
+	getLastestProductSpecListByPageNum = (productId,pageNum) => { //10 per page
+		let data = this.get(`/products/${productId}/specs/latest/pages/${pageNum}`);
+		return data;
+	}
+
+	getProductSpec = (productId,specId) => {
+		let data = this.get(`/products/${productId}/specs/${specId}/`);
+		return data;
+	}
+
+	createProductSpec = (productId, description, price, stock) => {
+		let data = this.post(`/products/${productId}/specs/create/`,{description, price, stock});
+		return data;
+	}
+
+	updateProductSpec = (productId, specId, description, price, stock) => {
+		let data = this.put(`/products/${productId}/specs/${specId}/`,{description, price, stock});
+		return data;
+	}
+
+	deleteProduct = (productId, specId) => {
+		let data = this.delete(`/products/${productId}/specs/${specId}/`);
+		return data;
+	}
+
+
+	//Customer CRUD
 	getCustomer = (id) => {
 		let data = this.get(`/customers/${id}/`);
 		return data;
@@ -92,6 +140,7 @@ class Api {
 		return data;
 	}
 
+	//Seller CRUD
 	getSeller = (id) => {
 		let data = this.get(`/sellers/${id}/`);
 		return data;
