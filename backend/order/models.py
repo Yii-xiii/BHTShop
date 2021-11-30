c43cb42c119e5b8981f59651c595b843a7cd7a96
Yii-xiii <yinxuann1357@gmail.com>
2021-11-30 21:13:05 +0800

    added order

---
+++ backend/order/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from product.models import Product
+from user.models import Customer
+from django.utils import timezone
+# Create your models here.
+
+class Order(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
+	customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=False)
+	quantity = models.IntegerField(null=False, blank=False)
+	totalPrice = models.DecimalField(decimal_places=2, max_digits=10)
+	address = models.TextField(blank=True)
+	phoneNumber = models.TextField(blank=True)
+	time = models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+		return self.customer.username + " bought " + self.product.title
+
+	def body(self):
+		return {'id': self.id,
+                'product' : self.product.body(),
+                'customer': self.customer.body(),
+                'quantity': self.quantity,
+                'totalPrice': self.totalPrice,
+                'address': self.address,
+                'phoneNumber': self.phoneNumber,
+                'time': self.time}
