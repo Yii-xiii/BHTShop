000e900982816c60e0d2cae0d4ad4187e2e2e8a6
Yii-xiii <yinxuann1357@gmail.com>
2021-11-30 11:47:57 +0800

    modified productSpecCreate url

--- backend/product/urls.py
+++ backend/product/urls.py
@@ -6,12 +6,15 @@ urlpatterns = [
     path('pages/<int:pageNum>', views.product_list_by_page),
     path('latest', views.latest_product_list),
     path('latest/pages/<int:pageNum>', views.latest_product_list_by_page),
+
     path('<int:pk>/', views.product),
     path('create/', views.create_product),
+
     path('<int:pk>/specs/', views.product_spec_list),
     path('<int:pk>/specs/pages/<int:pageNum>', views.product_spec_list_by_page),
     path('<int:pk>/specs/latest/', views.latest_product_spec_list),
     path('<int:pk>/specs/latest/pages/<int:pageNum>', views.latest_product_spec_list_by_page),
+
     path('<int:pk>/specs/<int:pk_spec>', views.product_spec),
-    path('<int:pk>/specs/create/', views.product_spec_list),
+    path('<int:pk>/specs/create/', views.create_product_spec),
 ]
