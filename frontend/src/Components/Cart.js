b62459b47dde44ea45e2c13fbbea229f82067ff7
Jeremy <tanchiachun2014@gmail.com>
2021-12-02 17:34:29 +0800

    added login before cart and fav

--- frontend/src/Components/Cart.js
+++ frontend/src/Components/Cart.js
@@ -3,10 +3,23 @@ import {Link} from 'react-router-dom'
 import cartLogo from './Cart.png'
 import Notification from './Notification'
 import './Cart.css'
+import Cookies from 'js-cookie'
 
 const Cart = ({count}) => {
+    const loggedInType = Cookies.get('user')
+
+    if (loggedInType === 'Customer') {
+        return (
+            <Link className='cart-link' to='/cart'>
+                <img className='cart-logo' src={cartLogo} alt='logo'/>
+                {/* getting cart count and pass in */}
+                <Notification count='0' type='cart'/>
+            </Link>
+        )
+    }
+    
     return (
-        <Link className='cart-link' to='/cart'>
+        <Link className='cart-link' to='/login'>
             <img className='cart-logo' src={cartLogo} alt='logo'/>
             {/* getting cart count and pass in */}
             <Notification count='0' type='cart'/>
