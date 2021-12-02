83190bae3bc64982b3b9f47aeb33e587ec361b15
Jeremy <tanchiachun2014@gmail.com>
2021-12-02 16:59:21 +0800

    fix display box

--- frontend/src/ProductPage.js
+++ frontend/src/ProductPage.js
@@ -2,16 +2,25 @@ import React from 'react'
 import {useParams} from 'react-router-dom'
 import ProductMainInfo from './Components/ProductMainInfo'
 import './ProductPage.css'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
 
 const ProductPage = () => {
     const { productId } = useParams()
 
     return (
-        <div className='product-page-box'>
-            <ProductMainInfo productId={ productId }/>
+        <div>
+            <Header />
 
-            <h1>TODO Item:{ productId } Descriptions</h1>
+            <div className='product-page-box'>
+                <ProductMainInfo productId={ productId }/>
+
+                <h1>TODO Item:{ productId } Descriptions</h1>
+            </div>
+
+            <Footer />
         </div>
+        
     )
 }
 
