d7a564bb8df0836c362c49ab1ecca2a04f8834b8
chinlam0814 <95013114+chinlam0814@users.noreply.github.com>
2021-12-02 14:56:45 +0800

    Rename frontend/src/PersonalInfoPage.js to frontend/src/Components/PersonalInfoPage.js

--- frontend/src/Components/PersonalInfoPage.js
+++ frontend/src/Components/PersonalInfoPage.js
@@ -6,31 +6,31 @@ const PersonalInfoPage = () => {
     return (
         <div className='personalinfopage'>
 			<div className="head">
-				<h1>个人信息</h1>
+				<h1>涓汉淇℃伅</h1>
 			</div>
 
 			<div className="text">
 				<h3>ID :</h3>
-				<h3>姓名 :</h3>
-				<h3>地址 :</h3>
-				<h3>电话 :</h3>
+				<h3>濮撳悕 :</h3>
+				<h3>鍦板潃 :</h3>
+				<h3>鐢佃瘽 :</h3>
 			</div>
 
 			<div className="textinfo">
 				<div class="idinfo">
-					<h4>输出ID</h4>
+					<h4>杈撳嚭ID</h4>
 					// getid
 				</div>
 				<div className="nameinfo">
-					<h4>输出姓名</h4>
+					<h4>杈撳嚭濮撳悕</h4>
 					// getname
 				</div>
 				<div className="addinfo">
-					<h4>输出地址</h4>
+					<h4>杈撳嚭鍦板潃</h4>
 					// getadd
 				</div>
 				<div className="phoneinfo">
-					<h4>输出电话</h4>
+					<h4>杈撳嚭鐢佃瘽</h4>
 					//getphone
 				</div>
 			</div>
