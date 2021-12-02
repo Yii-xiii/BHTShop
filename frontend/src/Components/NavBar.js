c1478ef9f7aa351076b31f7d676b02f371cd32e5
Jeremy <tanchiachun2014@gmail.com>
2021-12-02 17:51:50 +0800

    set limit to homepage for both seller and customer

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -4,8 +4,50 @@ import Logo from './Logo.png'
 import Operate from './Operate'
 import {Link} from 'react-router-dom'
 import Search from './Search'
+import Cookies from 'js-cookie'
 
 const NavBar = () => {
+    const loggedInType = Cookies.get('user')
+
+    if (loggedInType === 'Customer') {
+        return (
+            <header>
+                <nav className='navbar'>
+                    <Link className='home-logo' to='/'>
+                        <img className='navbar-logo' src={Logo} alt='logo'/>
+                    </Link>
+    
+                    <Search />
+    
+                    <div>
+                        <Operate className='operate-box'/>
+                    </div>
+                </nav>
+                <hr className='navbar-line'/>
+            </header>
+        )
+    } else if (loggedInType === 'Seller') {
+        const userId = Cookies.get('user_id')
+        const path = `/seller/${userId}`
+
+        return (
+            <header>
+                <nav className='navbar'>
+                    <Link className='home-logo' to={path}>
+                        <img className='navbar-logo' src={Logo} alt='logo'/>
+                    </Link>
+    
+                    <Search />
+    
+                    <div>
+                        <Operate className='operate-box'/>
+                    </div>
+                </nav>
+                <hr className='navbar-line'/>
+            </header>
+        )
+    }
+
     return (
         <header>
             <nav className='navbar'>
