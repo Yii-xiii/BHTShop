8441313b721018e95d56682736e1514a1edebc28
Yii-xiii <yinxuann1357@gmail.com>
2021-12-02 17:45:57 +0800

    fixed bug

--- backend/user/views.py
+++ backend/user/views.py
@@ -146,7 +146,7 @@ def user_login(request):
 		login(request, user)
 		try:
 			seller = Seller.objects.get(username = username)
-			return returnJson([dict(seller.body())], 0, {'user' : 'Seller', 'username' : username, 'id' : user.id})
+			return returnJson([dict(seller.body())], 0, {'user' : 'Seller', 'username' : username, 'user_id' : user.id})
 		except Seller.DoesNotExist:
 			try : 
 				customer = Customer.objects.get(username = username)
