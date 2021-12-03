c4a4495b17490f66a11e96457ea12c42e5d05458
chinlam0814 <95013114+chinlam0814@users.noreply.github.com>
2021-12-03 14:03:18 +0800

    Update PersonalInfoPage.js

--- frontend/src/PersonalInfoPage.js
+++ frontend/src/PersonalInfoPage.js
@@ -1,5 +1,6 @@
 import './PersonalInfoPage.css'
-import {useParams} from 'react-router-dom'
+
+import { useParams } from 'react-router-dom'
 import React from 'react'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
@@ -7,11 +8,12 @@ import Header from './Components/Header'
 const PersonalInfoPage = () => {
 	const { userId } = useParams()
 
+
     return (
 		<div>
 			<Header />
 
-			<div className='personalinfopage'>
+			<div>
 				<div className="head">
 					<h1>个人信息</h1>
 				</div>
@@ -26,19 +28,15 @@ const PersonalInfoPage = () => {
 				<div className="textinfo">
 					<div class="idinfo">
 						<h4>输出ID</h4>
-						// getid
 					</div>
 					<div className="nameinfo">
 						<h4>输出姓名</h4>
-						// getname
 					</div>
 					<div className="addinfo">
 						<h4>输出地址</h4>
-						// getadd
 					</div>
 					<div className="phoneinfo">
 						<h4>输出电话</h4>
-						//getphone
 					</div>
 				</div>
 			</div>
