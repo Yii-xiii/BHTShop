4abf8d848ecef660a57bfb64e9c93d2423a99019
Jeremy <tanchiachun2014@gmail.com>
2021-12-06 17:59:48 +0800

    added comment port

--- frontend/src/Components/ProductReviews.js
+++ frontend/src/Components/ProductReviews.js
@@ -1,7 +1,30 @@
 import React from 'react'
 import './ProductReviews.css'
+import { useParams } from 'react-router'
+import {useState, useEffect} from 'react'
+import api from './Api'
 
 const ProductReviews = () => {
+    const { productId } = useParams()
+    const [comments, setComments] = useState([])
+
+    const fetchComments = async() => {
+        const data = await api.getProductCommentList(productId)
+        // const data = await response.json()
+        
+        return data
+    }
+
+    // Importing data
+    useEffect(() => {
+        const getComments = async() => {
+            const productFromServer = await fetchComments()
+            
+        }
+
+        getComments()
+    }, [])
+
     return (
         <div className='product-review-outer-box'>
             <div className='review-title'>
