7320499f65a920b60f91d18cde7f969e5b59a460
Yii-xiii <yinxuann1357@gmail.com>
2021-12-08 15:37:55 +0800

    remove password from logging

--- backend/user/middleware/requestlog.py
+++ backend/user/middleware/requestlog.py
@@ -11,7 +11,10 @@ class RequestIdMiddleware(MiddlewareMixin):
 		# pass
 
 	def process_response(self, request, response):
-		if request.body:
+		print((response.read()))
+		if request.path == "/users/login/":
+			logger.info('{} : "{} {} , {}" {}'.format(request.user, request.method, request.path, dict(username=json.loads(request.body)["username"]), response.status_code))
+		elif request.body:
 			logger.info('{} : "{} {} , {}" {}'.format(request.user, request.method, request.path, json.loads(request.body), response.status_code))
 		else:
 			logger.info('{} : "{} {}" {}'.format(request.user, request.method, request.path, response.status_code))
