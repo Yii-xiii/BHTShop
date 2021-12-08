8751151169a09aab34995878ca3e888fc31b1684
Yii-xiii <yinxuann1357@gmail.com>
2021-12-08 16:41:02 +0800

    modified log for Admin page

--- backend/user/middleware/requestlog.py
+++ backend/user/middleware/requestlog.py
@@ -1,6 +1,7 @@
 from django.utils.deprecation import MiddlewareMixin
 import logging
 import json
+import re
 
 logger = logging.getLogger("test")
 
@@ -12,9 +13,13 @@ class RequestIdMiddleware(MiddlewareMixin):
 
 	def process_response(self, request, response):
 		# print(urlopen(response).read())
-		if request.path == "/users/login/":
+		# print(request.path)
+		# print(re.search("^(/admin/)",request.path))
+		if re.search("^(/admin/)",request.path):
+			logger.info('{} : "{} {}" {}'.format(request.user, request.method, request.path, response.status_code))
+		elif request.path == "/users/login/":
 			logger.info('{} : "{} {} , {}" {}'.format(request.user, request.method, request.path, dict(username=json.loads(request.body)["username"]), response.status_code))
-		elif request.body:
+		elif request.body is not None and len(request.body) != 0:
 			logger.info('{} : "{} {} , {}" {}'.format(request.user, request.method, request.path, json.loads(request.body), response.status_code))
 		else:
 			logger.info('{} : "{} {}" {}'.format(request.user, request.method, request.path, response.status_code))
