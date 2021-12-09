2928648adfe65548de3485572219cdc1795f05ce
Yii-xiii <yinxuann1357@gmail.com>
2021-12-09 13:33:56 +0800

    added category to search bar

--- frontend/src/SearchPage.js
+++ frontend/src/SearchPage.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
 import { useParams } from 'react-router'
+import { useNavigation , Link } from 'react-router'
+import api from './Components/Api'
+import Search from './Components/Search'
 
 const SearchPage = () => {
-    const { searchName } = useParams()
+    const {search, category} = useParams()
+    // const [page, setPage] = useState(1)
+    console.log(search)
+    console.log(category)
 
     return (
         <div>
             <Header />
-                Searching for {searchName}
+                Searching for 
+
             <Footer />
         </div>
     )
-}
+}   
 
 export default SearchPage
