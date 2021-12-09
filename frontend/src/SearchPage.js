ee794427767041510f87cf08e3c1b1f29df69775
Yii-xiii <yinxuann1357@gmail.com>
2021-12-09 20:56:31 +0800

    added searching img

--- frontend/src/SearchPage.js
+++ frontend/src/SearchPage.js
@@ -9,6 +9,7 @@ import Product from './Components/Product'
 
 const SearchPage = () => {
     const inParams = useParams()
+    const [status, setStatus] = useState(0)
     
     const [products,setProducts] = useState([])
     const [page, setPage] = useState('1')
@@ -19,6 +20,7 @@ const SearchPage = () => {
             if (inParams.keyword === 'none') {
                 const data = await api.randomProductListByCategory(inParams.category)
                 setProducts(data.data)
+                setStatus(1)
                 setTitle(inParams.category)
 
                 if (inParams.category === 'women clothes') {
@@ -39,11 +41,13 @@ const SearchPage = () => {
             } else if (inParams.category === 'none') {
                 const data = await api.searchProduct(page, inParams.keyword)
                 setProducts(data.data)
+                setStatus(1)
                 setTitle(inParams.keyword)
                 setTitle('查找有关' + inParams.keyword + '的商品')
             } else if (inParams.keyword !== 'none' && inParams.category !== 'none') {
                 const data = await api.searchProductByCategory(page,inParams.category,inParams.keyword)
                 setProducts(data.data)
+                setStatus(1)
                 setTitle('查找有关' + inParams.keyword + '的商品')
             }
         }
@@ -56,17 +60,27 @@ const SearchPage = () => {
             <Header />
 
             <div className='search-products-box'>
+                {status === 0? <h1> 请稍等。。。</h1>  : ''}
                 <h1>{title}</h1>
 
                 <div className='search-product-out-box'>
-                    {products.length === 0 ? 
+                    {status === 0 ? 
+                    <div>
+                        <div className='search-result-not-found-box'>
+                            <img src={NotFound} alt='img' className="searching-image"/>
+                            <h3>查找中。。。</h3> 
+                        </div>
+                    </div>
+                    : ''}
+                    
+                    {status === 1 && products.length === 0 ? 
                     <div className='search-result-not-found-box'>
-                        <img src={NotFound} alt='img'/>
+                        <img src={NotFound} alt='img' className="fade-in"/>
                         <h3>找不到结果</h3> 
                     </div>
                     : ''}
 
-                    {products.length > 0 ? 
+                    {status === 1 && products.length > 0 ? 
                     <div className='search-result-found-box'>
                         <div className='search-result-found-items-box'>
                             {products.map((product, index) => (
