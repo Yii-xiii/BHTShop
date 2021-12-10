ccd4b4fbc15d3f66a12cdfde41de76c42c94bcc1
Yii-xiii <yinxuann1357@gmail.com>
2021-12-10 16:01:25 +0800

    added pageCount

--- backend/followship/views.py
+++ backend/followship/views.py
@@ -8,10 +8,10 @@ import json
 # Create your views here.
 
 
-def returnJson(data=None, errorCode=0):
+def returnJson(data=None, pageCount=0, errorCode=0):
 	if data is None:
 		data = []
-	return JsonResponse({'errorCode': errorCode, 'data': data})
+	return JsonResponse({'errorCode': errorCode, 'data': data, 'pageCount' : int(pageCount)})
 
 # followship
 @login_required
@@ -19,7 +19,7 @@ def customer_followship_list(request):
 	try:
 		customer = Customer.objects.get(id=request.user.id)
 	except Customer.DoesNotExist:
-		return returnJson([], 403)
+		return returnJson([],0, 403)
 
 	followships = Followship.objects.filter(customer=customer)
 
@@ -31,7 +31,7 @@ def latest_customer_followship_list(request):
 	try:
 		customer = Customer.objects.get(id=request.user.id)
 	except Customer.DoesNotExist:
-		return returnJson([], 403)
+		return returnJson([],0, 403)
 
 	followships = Followship.objects.filter(customer=customer).order_by('-id')
 
@@ -42,11 +42,13 @@ def latest_customer_followship_list_by_page(request, pageNum):
 	try:
 		customer = Customer.objects.get(id=request.user.id)
 	except Customer.DoesNotExist:
-		return returnJson([], 403)
+		return returnJson([],0, 403)
 
-	followships = Followship.objects.filter(customer=customer).order_by('-id')[((pageNum-1)*10):(pageNum*10)]
+	followships = Followship.objects.filter(customer=customer)
+	pages = followships.count()
+	followships = followships.order_by('-id')[((pageNum-1)*10):(pageNum*10)]
 
-	return returnJson([dict(followship.body()) for followship in followships])
+	return returnJson([dict(followship.body()) for followship in followships], pages)
 
 
 @login_required
@@ -54,14 +56,14 @@ def create_customer_followship(request):
 	try:
 		customer = Customer.objects.get(id=request.user.id)
 	except Customer.DoesNotExist:
-		return returnJson([], 403)
+		return returnJson([],0, 403)
 
 	data = json.loads(request.body)
 
 	try:
 		seller = Seller.objects.get(id = data["sellerId"])
 	except Seller.DoesNotExist:
-		return returnJson([], 404)
+		return returnJson([],0, 404)
 
 	try:
 		followship = Followship.objects.get(seller=seller, customer=customer)
@@ -69,7 +71,7 @@ def create_customer_followship(request):
 		followship = Followship.objects.create(seller=seller, customer=customer)
 		return returnJson([dict(followship.body())])
 
-	return returnJson([],400)
+	return returnJson([],0,400)
 
 
 @login_required
@@ -77,17 +79,17 @@ def get_customer_followship(request, pk_seller):
 	try:
 		customer = Customer.objects.get(id=request.user.id)
 	except Customer.DoesNotExist:
-		return returnJson([], 403)
+		return returnJson([],0, 403)
 
 	try:
 		seller = Seller.objects.get(id = pk_seller)
 	except Seller.DoesNotExist:
-		return returnJson([], 404)
+		return returnJson([],0, 404)
 
 	try:
 		followship = Followship.objects.get(customer=customer, seller=seller)
 	except Followship.DoesNotExist:
-		return returnJson([], 404)
+		return returnJson([],0, 404)
 
 	return returnJson([dict(followship.body())])
 
@@ -97,17 +99,17 @@ def edit_customer_followship(request, pk_seller):
 	try:
 		customer = Customer.objects.get(id=request.user.id)
 	except Customer.DoesNotExist:
-		return returnJson([], 403)
+		return returnJson([],0, 403)
 
 	try:
 		seller = Seller.objects.get(id = pk_seller)
 	except Seller.DoesNotExist:
-		return returnJson([], 404)
+		return returnJson([],0, 404)
 
 	try:
 		followship = Followship.objects.get(customer=customer, seller=seller)
 	except Followship.DoesNotExist:
-		return returnJson([], 404)
+		return returnJson([],0, 404)
 
 	if request.method == 'DELETE':
 		followship.delete()
@@ -118,7 +120,7 @@ def seller_followship_count(request,pk):
 	try:
 		seller = Seller.objects.get(id = pk)
 	except Seller.DoesNotExist:
-		return returnJson([], 404)
+		return returnJson([],0, 404)
 
 	followship = Followship.objects.filter(seller=seller)
 	return returnJson(dict(count=len(followship)))
