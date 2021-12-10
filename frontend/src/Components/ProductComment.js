70a1ac37b170e881e6d01b300bdb15506c1d0760
Jeremy <tanchiachun2014@gmail.com>
2021-12-10 14:02:51 +0800

    added all comments function

--- frontend/src/Components/ProductComment.js
+++ frontend/src/Components/ProductComment.js
@@ -9,9 +9,41 @@ import { Rating } from '@mui/material'
 import ReportOutlinedIcon from '@mui/icons-material/ReportOutlined';
 import { useNavigate } from 'react-router'
 
-const ProductComment = ({ comment }) => {
+const ProductComment = ({ comment, type }) => {
     const navigate = useNavigate()
     const commentReportPath = `/report/comment/${comment.id}`
+
+    if (type === 'all-comments') {
+        return (
+            <div className='white-comment-out-box'>
+                <div className='comment-box'>
+                    <div className='comment-title'>
+                        <h4>{comment.order.customer.username}</h4>
+                        <span>购买了</span>
+                        <h4>{comment.order.productSpec.description}</h4>
+                    </div>
+                    
+                    <div className='comment-rating'>
+                        <Rating size='small' value={comment.rating} readOnly/>
+                    </div>
+                    
+                    <div className='comment-description'>
+                        {comment.description}   
+                    </div>
+                    
+                    <div className='comment-time'>
+                        {comment.time}
+                    </div>
+                </div>
+    
+                <div>
+                    <button className='comment-report-button' onClick={() => navigate(commentReportPath)}> 
+                        <ReportOutlinedIcon />
+                    </button>
+                </div>
+            </div>
+        )
+    }
 
     return (
         <div className='comment-out-box'>
--- frontend/src/ProductCommentsPage.js
+++ frontend/src/ProductCommentsPage.js
@@ -4,43 +4,78 @@ import Footer from './Components/Footer'
 import Header from './Components/Header'
 import {useState, useEffect} from 'react'
 import api from './Components/Api'
-import { Pagination } from '@mui/material'
+import { FormControlLabel, Pagination, Radio, RadioGroup } from '@mui/material'
+import ProductComment from './Components/ProductComment'
+import './ProductCommentsPage.css'
 
 const ProductCommentsPage = () => {
     const { productId } = useParams()
-    const [products, setProducts] = useState([])
     const [page, setPage] = useState(1)
+    const [comments, setComments] = useState([])
+    const [filter, setFilter] = useState('All')
 
-    const handleChange = (event, value) => {
-        setPage(value)
+    const handleFilterChange = (event, value) => {
+        setFilter(value)
     }
 
-    const fetchProducts = async() => {
-        const data = await api.getProductListByPage(page) 
-        console.log(data.data)
-        return data.data
+    const fetchComments = async() => {
+        if (filter === 'All') {
+            const data = await api.getProductCommentList(productId)
+            return data.data
+        } else if (filter === '5Star') {
+            const data = await api.getProductCommentListByRating(productId, 5)
+            return data.data
+        } else if (filter === '4Star') {
+            const data = await api.getProductCommentListByRating(productId, 4)
+            return data.data
+        } else if (filter === '3Star') {
+            const data = await api.getProductCommentListByRating(productId, 3)
+            return data.data
+        } else if (filter === '2Star') {
+            const data = await api.getProductCommentListByRating(productId, 2)
+            return data.data
+        } else if (filter === '1Star') {
+            const data = await api.getProductCommentListByRating(productId, 1)
+            return data.data
+        }
     }
-
+    
     useEffect(() => {
-        const getProducts = async() => {
-            const productsFromServer = await fetchProducts()
-            setProducts(productsFromServer)
+        const getComments = async() => {
+            const commentsFromServer = await fetchComments()
+            setComments(commentsFromServer)
         }
 
-        getProducts()
-    }, [page])
+        getComments()
+    }, [filter])
 
     return (
         <div>
             <Header />
-            <Pagination count={10} page={page} onChange={handleChange} />
-            <h1>{page}</h1>
-            {
-                products.map((product, index) => (
-                    <span>{product.title}</span>
-                ))
-            }
-             <h1>TODO PRODUCT {productId} COMMENTS</h1>
+            
+            <h1 className='all-comments-header-title'>所有评价</h1>
+
+            <RadioGroup value={filter} onChange={handleFilterChange} row className="row-radio-buttons-group">
+                <div className='row-radio-buttons'>
+                    <FormControlLabel value="All" control={<Radio />} labelPlacement="start" label="全部" />
+                    <FormControlLabel value="5Star" control={<Radio />} labelPlacement="start" label="5星" className='row-radio-button'/>
+                    <FormControlLabel value="4Star" control={<Radio />} labelPlacement="start" label="4星" className='row-radio-button'/>
+                    <FormControlLabel value="3Star" control={<Radio />} labelPlacement="start" label="3星" className='row-radio-button'/>
+                    <FormControlLabel value="2Star" control={<Radio />} labelPlacement="start" label="2星" className='row-radio-button'/>
+                    <FormControlLabel value="1Star" control={<Radio />} labelPlacement="start" label="1星" className='row-radio-button'/>
+                </div>
+                
+            </RadioGroup>
+
+            <div className='all-comments-show-box'>
+                {
+                    comments.length > 0 ? comments.map((comment, index) => (
+                        <ProductComment key={index} comment={comment} type='all-comments'/>
+                    )) : '' 
+                }
+            </div>
+            
+
             <Footer />
         </div>
     )
