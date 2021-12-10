93569540959d3fe909b2ddb8bd36a24b0013891e
Jeremy <tanchiachun2014@gmail.com>
2021-12-10 18:31:47 +0800

    update admin

--- frontend/src/AdminPage.js
+++ frontend/src/AdminPage.js
@@ -3,13 +3,17 @@ import AdminGraph from './Components/AdminGraph'
 import AdminWorkspace from './Components/AdminWorkspace'
 import Footer from './Components/Footer'
 import Header from './Components/Header'
+import './AdminPage.css'
 
 const AdminPage = () => {
     return (
         <div>
             <Header />
             <AdminGraph />
-            <AdminWorkspace />
+            <div className='admin-page-workspace-limit-box'>
+                <AdminWorkspace />
+            </div>
+            
             <Footer />
         </div>
     )
