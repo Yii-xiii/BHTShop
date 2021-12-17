ef0e7261f3eef6e71b92a23f20a58a768d4c4539
Yii-xiii <yinxuan99@outlook.com>
2021-12-17 07:54:19 -0800

    rm logging

--- backend/user/middleware/requestlog.py
+++ backend/user/middleware/requestlog.py
@@ -15,24 +15,24 @@ class RequestIdMiddleware(MiddlewareMixin):
 		# print(response.headers)
 		# print(request.path)
 		# print(re.search("^(/admin/)",request.path))
-		if request.method == "POST":
-			logger.info('{} : "{} {}" response_code:{}'.format(request.user, request.method, request.path, response.status_code))
-		elif re.search("^(/admin/)",request.path):
-			logger.info('{} : "{} {}" response_code:{}'.format(request.user, request.method, request.path, response.status_code))
-		elif request.path == "/users/login/":			
-			if response.headers["Content-Type"] == "application/json": 
-				logger.info('{} : "{} {} , {}" response_code:{} return_code:{}'.format(request.user, request.method, request.path, dict(username=json.loads(request.body)["username"]), response.status_code, json.loads(response.content)["errorCode"]))
-			else:
-				logger.info('{} : "{} {} , {}" response_code:{}'.format(request.user, request.method, request.path, dict(username=json.loads(request.body)["username"]), response.status_code))
-		elif request.body is not None and len(request.body) != 0:
-			if response.headers["Content-Type"] == "application/json": 
-				logger.info('{} : "{} {} , {}" response_code:{} return_code:{}'.format(request.user, request.method, request.path, json.loads(request.body), response.status_code, json.loads(response.content)["errorCode"]))
-			else:
-				logger.info('{} : "{} {} , {}" response_code:{}'.format(request.user, request.method, request.path, json.loads(request.body), response.status_code))
-		else:
-			if response.headers["Content-Type"] == "application/json": 
-				logger.info('{} : "{} {}" response_code:{} return_code:{}'.format(request.user, request.method, request.path, response.status_code, json.loads(response.content)["errorCode"]))
-			else:
-				logger.info('{} : "{} {}" response_code:{}'.format(request.user, request.method, request.path, response.status_code))
+		# if request.method == "POST":
+		# 	logger.info('{} : "{} {}" response_code:{}'.format(request.user, request.method, request.path, response.status_code))
+		# elif re.search("^(/admin/)",request.path):
+		# 	logger.info('{} : "{} {}" response_code:{}'.format(request.user, request.method, request.path, response.status_code))
+		# elif request.path == "/users/login/":			
+		# 	if response.headers["Content-Type"] == "application/json": 
+		# 		logger.info('{} : "{} {} , {}" response_code:{} return_code:{}'.format(request.user, request.method, request.path, dict(username=json.loads(request.body)["username"]), response.status_code, json.loads(response.content)["errorCode"]))
+		# 	else:
+		# 		logger.info('{} : "{} {} , {}" response_code:{}'.format(request.user, request.method, request.path, dict(username=json.loads(request.body)["username"]), response.status_code))
+		# elif request.body is not None and len(request.body) != 0:
+		# 	if response.headers["Content-Type"] == "application/json": 
+		# 		logger.info('{} : "{} {} , {}" response_code:{} return_code:{}'.format(request.user, request.method, request.path, json.loads(request.body), response.status_code, json.loads(response.content)["errorCode"]))
+		# 	else:
+		# 		logger.info('{} : "{} {} , {}" response_code:{}'.format(request.user, request.method, request.path, json.loads(request.body), response.status_code))
+		# else:
+		# 	if response.headers["Content-Type"] == "application/json": 
+		# 		logger.info('{} : "{} {}" response_code:{} return_code:{}'.format(request.user, request.method, request.path, response.status_code, json.loads(response.content)["errorCode"]))
+		# 	else:
+		# 		logger.info('{} : "{} {}" response_code:{}'.format(request.user, request.method, request.path, response.status_code))
 
 		return response
