faa138aa9ad9f6f3ffa4e903b901266f1f3b8fbf
Jeremy <tanchiachun2014@gmail.com>
2021-12-29 12:21:41 +0800

    update routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 import LoginPage from './LoginPage'
 import HomePage from './HomePage'
 import FavProductPage from './FavProductPage'
@@ -26,8 +26,123 @@ import SearchPage from './SearchPage';
 import ReportSuccessPage from './ReportSuccessPage';
 import EditProduct from './EditProduct';
 import PostmanHomePage from './PostmanHomePage';
+import Cookies from 'js-cookie';
 
 function App() {
+  let loggedIn = Cookies.get('user')
+
+  if (loggedIn !== undefined) {
+    if (loggedIn === 'Customer') {
+      return (
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path='/' element={<HomePage />}/>
+              <Route path='/search/:keyword/:category' element={<SearchPage />}/>
+              <Route path='/register' element={<Navigate to="/" />}/>
+              <Route path='/login' element={<Navigate to="/" />}/>
+              <Route path='/favProducts' element={<FavProductPage />}/>
+              <Route path='/favSellers' element={<FavSellerPage />}/>
+              <Route path='/cart' element={<CartPage />}/>
+              <Route path='/addProduct' element={<Navigate to="/" />}/>
+              <Route path='/sOrders/paid' element={<Navigate to="/" />}/>
+              <Route path='/sOrders/shipped' element={<Navigate to="/" />}/>
+              <Route path='/sOrders/delivered' element={<Navigate to="/" />}/>
+              <Route path='/sOrders/returning' element={<Navigate to="/" />}/>
+              <Route path='/sOrders/returned' element={<Navigate to="/" />}/>
+              <Route path='/sellerHome' element={<Navigate to="/" />}/>
+              <Route path='/product/:productId' element={<ProductPage />}/>
+              <Route path='/profile' element={<ProfilePage />}/>
+              <Route path='/admin' element={<Navigate to="/" />}/>
+              <Route path='/sellerProfile' element={<Navigate to="/" />}/>
+              <Route path='/seller/:sellerId' element={<SellerPage />}/>
+              <Route path='/sellerAnalysis' element={<Navigate to="/" />}/>
+              <Route path='/orders' element={<PersonalOrdersPage />}/>
+              <Route path='/order/:orderId' element={<OrderPage />}/>
+              <Route path='/product/:productId/comments' element={<ProductCommentsPage />}/>
+              <Route path='/product/:productId/edit' element={<EditProduct />}/>
+              <Route path='/report/:reportType/:reportingId' element={<ReportPage />}/>
+              <Route path='/report/:reportType/:reportingId/success' element={<ReportSuccessPage />}/>
+              <Route path='/postHome' element={<Navigate to="/" />}/>
+            </Routes>
+          </div>
+        </Router>
+      );
+    } else if (loggedIn === 'Seller') {
+      return (
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path='/' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/search/:keyword/:category' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/register' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/login' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/favProducts' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/favSellers' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/cart' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/addProduct' element={<AddProductPage />}/>
+              <Route path='/sOrders/paid' element={<SellerOrdersPaidPage />}/>
+              <Route path='/sOrders/shipped' element={<SellerOrdersShippedPage />}/>
+              <Route path='/sOrders/delivered' element={<SellerOrdersDeliveredPage />}/>
+              <Route path='/sOrders/returning' element={<SellerOrdersReturningPage />}/>
+              <Route path='/sOrders/returned' element={<SellerOrdersReturnedPage />}/>
+              <Route path='/sellerHome' element={<SellerHomePage />}/>
+              <Route path='/product/:productId' element={<ProductPage />}/>
+              <Route path='/profile' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/admin' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/sellerProfile' element={<ProfilePage />}/>
+              <Route path='/seller/:sellerId' element={<SellerPage />}/>
+              <Route path='/sellerAnalysis' element={<SellerAnalysisPage />}/>
+              <Route path='/orders' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/order/:orderId' element={<Navigate to="/sellerHome" />}/>
+              <Route path='/product/:productId/comments' element={<ProductCommentsPage />}/>
+              <Route path='/product/:productId/edit' element={<EditProduct />}/>
+              <Route path='/report/:reportType/:reportingId' element={<ReportPage />}/>
+              <Route path='/report/:reportType/:reportingId/success' element={<ReportSuccessPage />}/>
+              <Route path='/postHome' element={<Navigate to="/sellerHome" />}/>
+            </Routes>
+          </div>
+        </Router>
+      );
+    } else if (loggedIn === 'Postman') {
+      return (
+        <Router>
+          <div className="App">
+            <Routes>
+              <Route path='/' element={<Navigate to="/postHome" />}/>
+              <Route path='/search/:keyword/:category' element={<Navigate to="/postHome" />}/>
+              <Route path='/register' element={<Navigate to="/postHome" />}/>
+              <Route path='/login' element={<Navigate to="/postHome" />}/>
+              <Route path='/favProducts' element={<Navigate to="/postHome" />}/>
+              <Route path='/favSellers' element={<Navigate to="/postHome" />}/>
+              <Route path='/cart' element={<Navigate to="/postHome" />}/>
+              <Route path='/addProduct' element={<Navigate to="/postHome" />}/>
+              <Route path='/sOrders/paid' element={<Navigate to="/postHome" />}/>
+              <Route path='/sOrders/shipped' element={<Navigate to="/postHome" />}/>
+              <Route path='/sOrders/delivered' element={<Navigate to="/postHome" />}/>
+              <Route path='/sOrders/returning' element={<Navigate to="/postHome" />}/>
+              <Route path='/sOrders/returned' element={<Navigate to="/postHome" />}/>
+              <Route path='/sellerHome' element={<Navigate to="/postHome" />}/>
+              <Route path='/product/:productId' element={<Navigate to="/postHome" />}/>
+              <Route path='/profile' element={<ProfilePage />}/>
+              <Route path='/admin' element={<Navigate to="/postHome" />}/>
+              <Route path='/sellerProfile' element={<Navigate to="/postHome" />}/>
+              <Route path='/seller/:sellerId' element={<Navigate to="/postHome" />}/>
+              <Route path='/sellerAnalysis' element={<Navigate to="/postHome" />}/>
+              <Route path='/orders' element={<Navigate to="/postHome" />}/>
+              <Route path='/order/:orderId' element={<Navigate to="/postHome" />}/>
+              <Route path='/product/:productId/comments' element={<Navigate to="/postHome" />}/>
+              <Route path='/product/:productId/edit' element={<Navigate to="/postHome" />}/>
+              <Route path='/report/:reportType/:reportingId' element={<Navigate to="/postHome" />}/>
+              <Route path='/report/:reportType/:reportingId/success' element={<Navigate to="/postHome" />}/>
+              <Route path='/postHome' element={<PostmanHomePage />}/>
+            </Routes>
+          </div>
+        </Router>
+      );
+    }
+  }
+
   return (
     <Router>
       <div className="App">
